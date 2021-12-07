c045539bfe30e8eb33e5215fd7b5995690137d8c
Pedro Caridade <pedrocaridade@MAC030160-MiguelAlcantara.local>
2021-12-07 08:24:27 +0000

    Add UICollectionViewDataSource

--- GallerySearch/GalleryPhotosViewController.swift
+++ GallerySearch/GalleryPhotosViewController.swift
@@ -16,4 +16,76 @@ final class GalleryPhotosViewController: UICollectionViewController {
         left: 20.0,
         bottom: 50.0,
         right: 20.0)
+    //Keeps track of all searches made
+    private var searches: [GallerySearchResults] = []
+    //Reference to the object that searches
+    private let gallery = Gallery()
+    private let itemsPerRow: CGFloat = 3
+}
+
+private extension GalleryPhotosViewController {
+    //Gets a specific photo related to an index path in your collection view
+    func photo(for indexPath: IndexPath) -> GalleryPhoto {
+        return searches[indexPath.section].searchResults[indexPath.row]
+    }
+}
+
+//MARK: - Text Field Delegate
+
+extension GalleryPhotosViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        guard
+            let text = textField.text,
+            !text.isEmpty
+        else { return true }
+        //I use the Gallery wrapper class to search photos that match the given search term
+        let activityIndicator = UIActivityIndicatorView(style: .gray)
+        textField.addSubview(activityIndicator)
+        activityIndicator.frame = textField.bounds
+        activityIndicator.startAnimating()
+        
+        gallery.searchGallery(for: text) { searchResults in
+            DispatchQueue.main.async {
+                activityIndicator.removeFromSuperview()
+                
+                switch searchResults {
+                case .failure(let error) :
+                    //Log any error to the console
+                    print("Error Searching: \(error)")
+                case .success(let results):
+                    //Log the results and add them at the beginning of searches array
+                    print("""
+                        Found \(results.searchResults.count) \
+                        matching \(results.searchTerm)
+                        """)
+                    self.searches.insert(results, at: 0)
+                    //Refresh to show the new data
+                    self.collectionView?.reloadData()
+                }
+            }
+        }
+        
+        textField.text = nil
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+//MARK: - UICollectionViewDataSource
+
+extension GalleryPhotosViewController {
+    //One search per section, the number of sections is the count of searches
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return searches.count
+    }
+    //The number of items in a section is the count of searchResults
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return searches[section].searchResults.count
+    }
+    //Placeholder method to return a blank cell, if i didin't register a cell with reuseIdentifier it will cause a runtime error
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = .black
+        return cell
+    }
 }
